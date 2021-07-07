943f81b6f5668dfa67080fd34e5388882be9a1f5
yousaf-coder <77007388+yousaf-coder@users.noreply.github.com>
2021-07-07 20:11:44 +0500

    yousaf changes

--- src/views/pages/employees/TaxInfo.vue
+++ src/views/pages/employees/TaxInfo.vue
@@ -3,7 +3,7 @@
     <div class="p-fluid">
       <div class="p-field p-grid">
         <label for="firstname4" class="p-col-12 p-mb-2 p-md-6 p-mb-md-0"
-          >Federal Exempt</label
+          >Federal Exempt </label
         >
         <div class="p-col-12 p-md-6">
           <div class="check-box">
